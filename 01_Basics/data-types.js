7a6d9a944e08bf1717f590bfe1ab74f9f3475cd4
Anuragsinghpy1 <63054225+Anuragsinghpy1@users.noreply.github.com>
2025-07-03 12:55:05 +0000

    commit data type

---
+++ 01_Basics/data-types.js
@@ -0,0 +1,29 @@
+"Use Strict" // treat as a new JS version;
+
+// alert("Anurag") // not work on the node file (but we have some method to run on node) and work only brower
+
+// Data Types //
+
+// String => "text part"
+// Number => 22
+// Boolean => true and false
+// BigInt => Big Number (use for large values)
+// null =>
+// undefined =>
+
+let nametype = "Anurag"; // string
+let age = 25; // number
+let attendence = true; // boolean
+let attendenceReport = null; // Null is a object
+let attendenceData; // Undefined
+
+console.log(typeof age);
+console.table([typeof nametype, typeof age, typeof attendence, typeof attendenceReport, typeof attendenceData])
+
+let firstname = "Anuragpy1"
+let lastname = "singh"
+let marital = "Single"
+let countryName = "India"
+let agenew = 25
+
+console.log(`My name is ${firstname} ${lastname} and my martial status is ${marital} and my country name is ${countryName}`)
