16ff50a2dc3e38f50e8aa084792a980d7f4f045b
Anuragsinghpy1 <63054225+Anuragsinghpy1@users.noreply.github.com>
2025-07-03 11:10:36 +0000

    Commit Variables

---
+++ 01_Basics/Variables.js
@@ -0,0 +1,16 @@
+const accountId = '0001' // accountId -> Memory and 0001 -> Value
+let accountEmail = "anuragsinghpy1@gmail.com"
+var accountPasswords = "12345" // Don't use (var) variable
+accountCity = "Bengluru"
+let accountState; // Undefined
+
+// Change the variable values
+
+accountEmail = "anu@gmail.com"
+accountPasswords = "54321"
+accountCity = "Punjab"
+accountState = "Karnataka"
+
+console.table([accountId, accountEmail, accountPasswords, accountCity, accountState]);
+// console.log(accountEmail);
+
