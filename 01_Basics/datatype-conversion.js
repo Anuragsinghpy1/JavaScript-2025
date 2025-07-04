67ae57bba641c90fda27687205b5b4aebfc1553a
Anuragsinghpy1 <63054225+Anuragsinghpy1@users.noreply.github.com>
2025-07-04 05:15:12 +0000

    datatype conversion

--- 01_Basics/datatype-conversion.js
+++ 01_Basics/datatype-conversion.js
@@ -1,19 +1,32 @@
 let myscore = "33frk"
 
-console.log(typeof (myscore));
+// console.log(typeof (myscore));
 
-let myscoreInumber = Number(myscore)
-console.log(typeof myscoreInumber)
-console.log(myscoreInumber);
+// let myscoreInumber = Number(myscore)
+// console.log(typeof myscoreInumber)
+// console.log(myscoreInumber);
 
 // "33" => 33  
 
 let mynewscore = 99999
-console.log(typeof mynewscore);
+// console.log(typeof mynewscore);
 
-let mynewsscoreString = String(mynewscore);
-console.log(typeof mynewsscoreString);
+// let mynewsscoreString = String(mynewscore);
+// console.log(typeof mynewsscoreString);
 
-console.log(mynewsscoreString)
+// console.log(mynewsscoreString)
+
+let mynewscoreupdate = 1
+console.log(typeof mynewscoreupdate);
+
+let mynewscoreChange = Boolean(mynewscoreupdate);
+console.log(typeof mynewscoreChange);
+console.log(mynewscoreChange);
+
+
+
+
+
+
 
 
