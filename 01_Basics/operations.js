2047beb1c6b53f8e15a84b4811a8707a438ad525
Anuragsinghpy1 <63054225+Anuragsinghpy1@users.noreply.github.com>
2025-07-08 20:11:47 +0000

    Operations: Post and Prefix Increment

--- 01_Basics/operations.js
+++ 01_Basics/operations.js
@@ -2,21 +2,39 @@ let str1 = 1;
 let str2 = 5;
 
 let str3 = str1+str2;
-console.log(str3);
+// console.log(str3);
 
 ////////////////////////
 
- console.log(1 + '2');
- console.log('2' + 1);
- console.log('2' + '3');
- console.log(2 + '2' + '2');
- console.log(3 + 3 + "7");
+//  console.log(1 + '2');
+//  console.log('2' + 1);
+//  console.log('2' + '3');
+//  console.log(2 + '2' + '2');
+//  console.log(3 + 3 + "7");
 
  ////////////////////////////
 
  let gameCounter = 97;
  ++gameCounter
- console.log(gameCounter);
+//  console.log(gameCounter);
+
+
+ // Postfix increment //
+
+ let x = 4
+ let y = x++
+ console.log(`x: ${x} & y: ${y}`);
+
+
+ // Prefix increment
+
+ let x1 = 6;
+ const y1 = ++x1;
+ console.log(`x1: ${x1} & y1: ${y1}`);
+ 
+
+ 
+ 
  
 
 
