70f08fa870871f3d779a8d195f3ef77e73d31f5e
Anuragsinghpy1 <63054225+Anuragsinghpy1@users.noreply.github.com>
2025-07-08 19:46:04 +0000

    Baisc Operations

---
+++ 01_Basics/operations.js
@@ -0,0 +1,26 @@
+let str1 = 1;
+let str2 = 5;
+
+let str3 = str1+str2;
+console.log(str3);
+
+////////////////////////
+
+ console.log(1 + '2');
+ console.log('2' + 1);
+ console.log('2' + '3');
+ console.log(2 + '2' + '2');
+ console.log(3 + 3 + "7");
+
+ ////////////////////////////
+
+ let gameCounter = 97;
+ ++gameCounter
+ console.log(gameCounter);
+ 
+
+
+ 
+ 
+ 
+ 
