1be2d9908c8573cdc653363545cff029b0399659
Anuragsinghpy1 <63054225+Anuragsinghpy1@users.noreply.github.com>
2025-07-10 13:31:39 +0000

    Comparison practice

---
+++ 01_Basics/comparison.js
@@ -0,0 +1,43 @@
+//
+
+// let x = 5
+
+
+// console.log(3 == 9);
+// console.log(3 == "9");
+// console.log(3 === 9);
+// console.log(3 === "9");
+// console.log(3 !=9);
+// console.log(3 !="9");
+// console.log(3 > 9);
+// console.log(3 < 9);
+// console.log(3 <= 9);
+// console.log(3 >= 9);
+
+//  This kind of comparison is little confusing so carefull //
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+//
+
+console.log(undefined = 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
